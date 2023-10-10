93e07724101f506779ed1c343b9246218176c5de
NURSENA BİTİRGEN <94117345+senabibi@users.noreply.github.com>
2023-10-10 15:11:49 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from turtle import Screen
-from snake import Snake
-from food import Food
-from scoreboard import Scoreboard
 import time
+from food import Food
+from snake import Snake
+from scoreboard import Scoreboard
 
 screen=Screen()
 screen.setup(width=600,height=600)
@@ -21,33 +21,35 @@ screen.onkey(snake.left,"Left")
 screen.onkey(snake.right,"Right")
 
 
-
 game_is_on=True
+
 while game_is_on:
-    screen.update()
-    time.sleep(0.1)
-    snake.move()
+	screen.update()
+	time.sleep(0.1)
+	snake.move()
 
- 
-    if snake.head.distance(food)<15:
-        food.refresh()
-        snake.extend()
-        scoreboard.increase_score()
+	if snake.head.distance(food)<15:
+		food.refresh()
+		snake.extend()
+		scoreboard.increase_score()
 
-    if snake.head.xcor()>280 or snake.head.xcor()<-280 or snake.head.ycor()>280 or snake.head.ycor()<-280:
-            scoreboard.reset()
-            snake.reset()
-           #game_is_on=False
-           #scoreboard.game_over()
+	if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+		scoreboard.reset()
+		snake.reset()
+	for segm in snake.segment[1:]:
+		
+		
+			
+		if snake.head.distance(segm)<10:
+			scoreboard.reset()
+			snake.reset()
+		
+			
+		
+		
 
-    for segment in snake.segments[1:]:
-         if snake.head.distance(segment)<10:
-              #game_is_on=False
-             #scoreboard.game_over()
-             scoreboard.reset()
-             snake.reset()
-        
-    
-    
+	
 screen.exitonclick()
-
+		
+		
+		
